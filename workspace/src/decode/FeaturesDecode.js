cd33899001402f2d21ebbe5c1457295ff37ae23c
Eduardo Lopes <ef.lopes@campus.fct.unl.pt>
2019-06-03 11:55:21 +0100

    [ISSUE#10] Create Decode class for polygon / properties of layers
     Create Decode class for polygon / properties of layers
    
    URL: https://github.com/filipedu11/deeapp/issues/10

---
+++ workspace/src/decode/FeaturesDecode.js
@@ -0,0 +1,19 @@
+
+export class FeaturesDecode{
+
+    constructor(){
+
+        //GLOBAL VARIABLES
+        this.type = "type";
+        this.properties = "properties";
+        this.geometry = "geometry";
+
+        //properties
+        this.classId = "classId";
+        this.className = "className";
+        this.areaInHectare = "areaInHectare";
+        this.areaInPixels = "areaInPixels";
+        this.perimeterInMeters = "perimeterInMeters";
+        this.numVertices = "numVertices";
+    }
+}
