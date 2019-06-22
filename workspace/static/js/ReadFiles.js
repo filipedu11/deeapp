a6bc8ca6252b1394aef894d821d1555635a0aa0e
Eduardo Lopes <ef.lopes@campus.fct.unl.pt>
2019-06-22 19:10:36 +0100

    [NO_ISSUE] Remove unnecessary read file
    Remove unnecessary read file

--- workspace/static/js/ReadFiles.js
+++ workspace/static/js/ReadFiles.js
@@ -8,10 +8,6 @@ export class ReadFiles{
         return JSON.parse(readFileSync('./workspace/static/data/classification/classification_example.geojson', 'utf8').trim());
     }
 
-    readClassificationExample2(){
-        return JSON.parse(readFileSync('./workspace/static/data/classification/classification_example_2.geojson', 'utf8').trim());
-    }
-
     readCountries(){
         return JSON.parse(readFileSync('./workspace/static/data/countries.geojson', 'utf8').trim());
     }
