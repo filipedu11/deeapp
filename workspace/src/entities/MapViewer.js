af0a1f6354115c3000f24127bd938a2bef0f0ad3
Eduardo Lopes <ef.lopes@campus.fct.unl.pt>
2019-07-23 21:50:36 +0100

    [ISSUE#23] compute comparision between 2 layers
    compute comparision between 2 layers
    
    URL: https://github.com/filipedu11/deeapp/issues/21

--- workspace/src/entities/MapViewer.js
+++ workspace/src/entities/MapViewer.js
@@ -33,10 +33,16 @@ import 'highcharts/modules/offline-exporting.js';
 
 import Highcharts from 'highcharts';
 import * as turf from '@turf/turf';
+import RBush from 'geojson-rbush';
+
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+var geojsonRbush = require('geojson-rbush').default;
 
 var BASE_TYPE_STRING = 'basemap';
 var CLASSIFICATION_TYPE_STRING = 'classification';
 var VALIDATION_STRING = 'validation';
+var EVALUATION_STRING = 'evaluation';
 
 export class MapViewer{
 
@@ -117,9 +123,16 @@ export class MapViewer{
             typeBase: VALIDATION_STRING
         });
 
+        var evaluations = new LayerGroup({
+            title: 'Evaluations',
+            fold: 'open',
+            typeBase: EVALUATION_STRING
+        });
+
         this.map.addLayer(base);
         this.map.addLayer(classifications);
         this.map.addLayer(validations);
+        this.map.addLayer(evaluations);
     }
 
     addLayerToMapGroup(typeGroup, newLayer){
@@ -232,6 +245,37 @@ export class MapViewer{
         this.loadLayerSwitcher();
     }
 
+    addEvaluation(classiGeojson){
+
+        var cD = new ClassificationDecode();
+        var k = cD.key;
+
+        var id = classiGeojson[cD.classificationID[k]];
+
+        var classification = new Classification(
+            id,
+            classiGeojson[cD.classificationName[k]],
+            classiGeojson[cD.classificationDescription[k]],
+            classiGeojson[cD.classificationRasterFile[k]],
+            classiGeojson[cD.classificationSource[k]],
+            classiGeojson[cD.classificationStats[k]],
+            classiGeojson[cD.classificationStyle[k]],
+            classiGeojson[cD.features[k]],
+            classiGeojson[cD.classNames[k]],
+            classiGeojson,
+            EVALUATION_STRING
+        );
+
+        this.allLayersDict[id] = classification;
+        this.valiArray.push(classification);
+
+        var layer = this.createLayer(classiGeojson, classification);
+
+        this.addLayerToMapGroup(EVALUATION_STRING, layer);
+
+        this.loadLayerSwitcher();
+    }
+
 
     createStyle(lyr){
 
@@ -240,21 +284,23 @@ export class MapViewer{
         var fD = classAux.getDecode().featuresDecode;
         var k = classAux.getDecode().key;
 
-        var mapAux = this.map;
+        var inactiveC = lyr.get('inactiveClasses');
 
         lyr.setStyle(function name(feature, resolution) {
 
             var colorAux = classAux.getColorOfClass(feature.get(fD.classId[k]));
 
-            return [new Style({
-                // stroke: new Stroke({
-                //     color: 'rgba(255,255,255,0)',
-                //     width: 0
-                // }),
-                fill: new Fill({
-                    color: colorAux,
-                }),
-            })];
+            if ( !inactiveC[feature.get(fD.classId[k])] ) {
+                return [new Style({
+                    // stroke: new Stroke({
+                    //     color: 'rgba(255,255,255,0)',
+                    //     width: 0
+                    // }),
+                    fill: new Fill({
+                        color: colorAux,
+                    }),
+                })];
+            }
         });
     }
 
@@ -308,40 +354,12 @@ export class MapViewer{
 
         var layer = new VectorLayer({
             title: classObject.getName(),
-            visible: false,
+            visible: EVALUATION_STRING == classObject.getType(),
             source: source,
             layerId: classObject.getId(),
             sourceAux: vectorSource,
             inactiveClasses: {}
         });
-
-        layer.set(
-            'showHideClass',
-            function(lyr, classAux) {
-
-                var inactiveC = lyr.get('inactiveClasses');
-
-                lyr.setStyle(function name(feature, resolution) {
-
-                    var fD = classAux.getDecode().featuresDecode;
-                    var k = classAux.getDecode().key;
-
-                    var colorAux = classAux.getColorOfClass(feature.get(fD.classId[k]));
-
-                    if ( !inactiveC[feature.get(fD.classId[k])] ) {
-                        return [new Style({
-                            // stroke: new Stroke({
-                            //     color: 'rgba(255,255,255,0)',
-                            //     width: 0
-                            // }),
-                            fill: new Fill({
-                                color: colorAux,
-                            }),
-                        })];
-                    }
-                });
-            }
-        );
 
         this.createStyle(layer);
 
@@ -403,18 +421,100 @@ export class MapViewer{
 
             if (isEval) {
 
-                var features, intersetFeatures;
+                const el1 = this.lyrsSelected[0];
+                const el2 = this.lyrsSelected[1];
 
-                this.lyrsSelected.forEach(lyr => {
-                    var dataLyr = this.getObjectLayer(lyr.get('layerId'));
-                    features = turf.featureCollection(dataLyr['features']);
+                var tree = geojsonRbush();
 
-                    console.log(turf.combine(features));
+                var dataEl1 = this.getObjectLayer(el1.get('layerId'));
+                var geojson1 = dataEl1.geojsonFile;
 
-                    //intersetFeatures = intersetFeatures == undefined ? features : turf.collect(intersetFeatures, features, 'classId', 'classId');
-                });
+                var dataEl2 = this.getObjectLayer(el2.get('layerId'));
+                var geojson2 = dataEl2.geojsonFile;
 
-                console.log(intersetFeatures);
+                var rbush = geojson1.features.length > geojson2.features.length ? tree.load(geojson1) : tree.load(geojson2);
+                var features = geojson1.features.length > geojson2.features.length ? geojson2.features : geojson1.features;
+
+                var intersect = {
+                    'type': 'FeatureCollection',
+                    'name': 'out',
+                    'crs': {
+                        'type': 'name',
+                        'properties': {
+                            'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'
+                        }
+                    },
+                    'features': []
+                };
+                
+                var element = null;
+                var containElements = null;
+
+                for (let index = 0; index < 50/* features.length */; index++) {
+                    element = features[index];
+                    
+                    containElements = rbush.search(features[index]);
+
+                    if (containElements.features.length != 0) {
+            
+                        containElements.features.map();
+                        containElements.features.map(function(contEl){
+
+                            var feat = turf.intersect(element, contEl);
+
+                            if (feat != null) {
+                                feat.properties = {
+                                    'featureId': element['properties']['featureId'] + ' - ' + contEl['properties']['featureId'],
+                                    'classId': element['properties']['classId'] + '' + contEl['properties']['classId'],
+                                    'className': element['properties']['className'] + ' vs ' + contEl['properties']['className']
+                                };                                
+                                intersect.features.push(feat);
+                            }
+                        });
+                    }
+                }
+
+                const extIntersect = {
+                    'crs': {
+                        'type': 'name',
+                        'properties': {
+                            'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'
+                        }
+                    },
+                    'classificationID': dataEl1.getId() + dataEl2.getId(),
+                    'classificationName': dataEl1.getName() + ' vs ' + dataEl2.getName(),
+                    'classificationDescription': 'Dados de Validação que mapeia a area ardida',
+                    'classificationRasterFile': 'classification_example.tif',
+                    'classificationSource': {
+                        'author': 'Eduardo Fernandes',
+                        'classifierAlgorithm': '',
+                        'preProcTechniquesUsed': [],
+                        'postProcTechniquesUsed': [],
+                        'collectedDate': '',
+                        'classificationDate': ''
+                    },
+                    'classificationStyle': {
+                        'color': {
+                            '00': 'rgb(206,206,206)',
+                            '01': 'rgb(150,150,150)',
+                            '10': 'rgb(150,95,50)',
+                            '11': 'rgb(173,112,68)'
+                        }
+                    },
+                    'classNames': {
+                        '00': 'Área não ardida (Correct)',
+                        '01': 'Área não ardida (Wrong)',
+                        '10': 'Área ardida (Wrong)',
+                        '11': 'Área ardida (Correct)',
+                    }
+                };
+
+                intersect =  Object.assign({}, intersect, extIntersect);
+
+                intersect['type'] = 'FeatureCollection';
+
+                console.log(intersect);
+                this.addEvaluation(intersect);
             }
             else {
                 return;
@@ -428,6 +528,7 @@ export class MapViewer{
         var classNames = dataLyr.getClassNames();
         var classColors = dataLyr.getClassColors();
         var classKeys = dataLyr.getKeysOfClasses();
+
 
         var dataPie = [];
         var classIndex = {};
@@ -466,7 +567,7 @@ export class MapViewer{
                 text: dataLyr.getName()
             },
             tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b><br/>{series.name}: <b>{point.y:.3f} km</b>'
             },
             plotOptions: {
                 pie: {
@@ -480,10 +581,8 @@ export class MapViewer{
                         events: {
                             legendItemClick: function(e) {
                                 var C_ID = e['target']['id'];
-
                                 lyr.get('inactiveClasses')[C_ID] = !lyr.get('inactiveClasses')[C_ID];
-
-                                lyr.get('showHideClass')(lyr, dataLyr);
+                                lyr.getSource().dispatchEvent('change');
                             }
                         }
                     }
